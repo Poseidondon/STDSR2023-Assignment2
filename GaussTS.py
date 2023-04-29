8cc998d610624a8291803f4ddb755c8e1e3e4bb9
Poseidondon <superzarub@rambler.ru>
2023-04-29 11:46:36 +0300

    Gaussian Thompson sampling and UCB implemented.

---
+++ GaussTS.py
@@ -0,0 +1,62 @@
+"""
+Source:
+https://github.com/WhatIThinkAbout/BabyRobot/blob/master/Multi_Armed_Bandits/Part%205b%20-%20Thompson%20Sampling%20using%20Conjugate%20Priors.ipynb
+"""
+
+from scipy.stats import gamma, norm
+
+
+def ind_max(x):
+    m = max(x)
+    return x.index(m)
+
+
+class GaussTS:
+    def __init__(self, n_arms=3, alphas=None, betas=None, means=None, counts=None):
+        self.n_arms = n_arms
+        if alphas:
+            self.alphas = alphas
+        else:
+            self.alphas = [1 for _ in range(n_arms)]
+
+        if betas:
+            self.betas = betas
+        else:
+            self.betas = [10 for _ in range(n_arms)]
+
+        if means:
+            self.means = means
+        else:
+            self.means = [1 for _ in range(n_arms)]
+
+        self.vars = [self.betas[i] / (self.alphas[i] + 1) for i in range(n_arms)]
+
+        if counts:
+            self.counts = counts
+        else:
+            self.counts = [0 for _ in range(n_arms)]
+        return
+
+    def select_arm(self):
+        est_vars = [float('inf') for _ in range(self.n_arms)]
+        for arm in range(self.n_arms):
+            precision = gamma.rvs(self.alphas[arm], 1 / self.betas[arm])
+            if precision != 0 and self.counts[arm] != 0:
+                est_vars[arm] = 1 / precision
+
+        samples = [norm.rvs(self.means[arm], self.vars[arm] ** 0.5) for arm in range(self.n_arms)]
+
+        return ind_max(samples)
+
+    def update(self, arm, reward):
+        v = self.counts[arm]
+
+        self.alphas[arm] = self.alphas[arm] + 0.5
+        self.betas[arm] = self.betas[arm] + ((1 * v / (v + 1)) * (((reward - self.means[arm]) ** 2) / 2))
+
+        # estimate the variance - calculate the mean from the gamma hyper-parameters
+        self.vars[arm] = self.betas[arm] / (self.alphas[arm] + 1)
+
+        self.means[arm] = (self.means[arm] * self.counts[arm] + reward) / (v + 1)
+        self.counts[arm] += 1
+        return
